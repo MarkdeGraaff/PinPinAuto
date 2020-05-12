99314a22684d390e4b06fc29af45224c8800068b
MarkdeGraaff <d275386@edu.rocwb.nl>
2020-05-12 23:21:18 +0200

    toedeloe

--- PinPinAuto/PinPinAuto/MainWindow.xaml.cs
+++ PinPinAuto/PinPinAuto/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PinPinAuto
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("toedeloe");
+        }
     }
 }
